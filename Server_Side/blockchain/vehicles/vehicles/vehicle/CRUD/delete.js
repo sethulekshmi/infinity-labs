fbd2c9cfc4965dfed87ca3397b35b5b324339792
sethulekshmi <sethulekshmi777@gmail.com>
2017-03-06 14:44:07 +0530

    Update delete.js

--- Server_Side/blockchain/vehicles/vehicles/vehicle/CRUD/delete.js
+++ Server_Side/blockchain/vehicles/vehicles/vehicle/CRUD/delete.js
@@ -21,7 +21,7 @@ let update = function(req, res, next, usersToSecurityContext)
 
     let v5cID = req.params.v5cID;
 
-    tracing.create('ENTER', 'DELETE blockchain/assets/vehicles/vehicle/'+v5cID+'/scrap', {});
+    tracing.create('ENTER', 'DELETE blockchain/vehicles/vehicles/vehicle/'+v5cID+'/scrap', {});
 
     res.write('{"message":"Formatting request"}&&');
 
@@ -29,21 +29,21 @@ let update = function(req, res, next, usersToSecurityContext)
 
     return Util.invokeChaincode(securityContext, 'scrap_vehicle', [ v5cID ])
     .then(function(data) {
-        tracing.create('INFO', 'DELETE blockchain/assets/vehicles/vehicle/'+v5cID+'/scrap', 'Achieving consensus');
+        tracing.create('INFO', 'DELETE blockchain/vehicles/vehicles/vehicle/'+v5cID+'/scrap', 'Achieving consensus');
         res.write('{"message":"Achieving consensus"}&&');
         let result = {};
         result.message = 'Scrap updated';
-        tracing.create('EXIT', 'DELETE blockchain/assets/vehicles/vehicle/'+v5cID+'/scrap', result);
+        tracing.create('EXIT', 'DELETE blockchain/vehicles/vehicles/vehicle/'+v5cID+'/scrap', result);
         res.end(JSON.stringify(result));
     })
     .catch(function(err) {
         res.status(400);
-        tracing.create('ERROR', 'DELETE blockchain/assets/vehicles/vehicle/'+v5cID+'/scrap', 'Unable to update scrap. v5cID: '+ v5cID);
+        tracing.create('ERROR', 'DELETE blockchain/vehicles/vehicles/vehicle/'+v5cID+'/scrap', 'Unable to update scrap. v5cID: '+ v5cID);
         let error = {};
         error.error = true;
         error.message = 'Unable to update scrap. ' + err;
         error.v5cID = v5cID;
-        tracing.create('ERROR', 'DELETE blockchain/assets/vehicles/vehicle/'+v5cID+'/scrap', error);
+        tracing.create('ERROR', 'DELETE blockchain/vehicles/vehicles/vehicle/'+v5cID+'/scrap', error);
         res.end(JSON.stringify(error));
     });
 };
